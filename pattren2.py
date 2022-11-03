f0f2666d7537b841c9b407747f8e6e872f18b632
Rohit Kumbhar <101877080+rohitkumbhargithub@users.noreply.github.com>
2022-11-03 17:02:59 +0530

    Add files via upload

---
+++ pattren2.py
@@ -0,0 +1,9 @@
+#Numeric pattern using for loop
+
+n=int(input("Enter the Character A to Z(65 to 90): "))
+for i in range(0,5):
+   for j in range(0,i+1):
+     al=chr(n)
+     print(al, end=' ')
+     n+=1
+   print()
